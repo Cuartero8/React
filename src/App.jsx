daec234fbca763e2de94854133e8f345ad479006
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2024-03-07 22:00:04 +0100

    new MouseClicker

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import AlertClock from "./Basic Concepts/AlertClock"
 import Clock from "./Basic Concepts/Clock"
 import Counter from "./Basic Concepts/Counter"
 import Hello from "./Basic Concepts/Hello"
+import MouseClicker from "./Basic Concepts/MouseClicker"
 import Welcome from "./Basic Concepts/Welcome"
 
 function App() {
 
   return (
     <div>
-      
+      <MouseClicker />
     </div>
   )
 }
